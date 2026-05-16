6af9b163d708a5843b0586e6c4fa851235ed4718
SG HARSHAA <harshaavardhan8@gmail.com>
2026-05-16 23:27:42 +0530

    updated!!

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import ProductCard from "./components/ProductCard";
 
-import products from "./data/productsData";
+import products from "./Data/productsData";
 
 function App() {
   const [cartCount, setCartCount] = useState(0);
